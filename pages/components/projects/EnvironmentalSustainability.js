b46c5bd07318182261a1b95fbcadb68307565044
Bikash Jethy <58800928+Bikash168@users.noreply.github.com>
2025-01-31 13:51:13 +0530

    new

--- pages/components/projects/EnvironmentalSustainability.js
+++ pages/components/projects/EnvironmentalSustainability.js
@@ -1,9 +1,9 @@
 const EnvironmentalSustainability = () => {
     return (
       <div>
-        <h3 className="text-2xl font-semibold text-[#316b9e] mb-4">Environmental Sustainability Projects</h3>
+        <h3 className="text-2xl font-semibold text-[#316b9e] mb-4">Sub - Theme - 6</h3>
         <p className="text-lg text-gray-700">
-          CSR funding for ensuring environmental sustainability by implementing the sustainable rural and urban decarbonisation projects of energy, agriculture, and water management sectors.
+        CSR funding for ensuring environmental sustainability by implementing the sustainable rural and urban decarbonization projects of energy, agriculture and water management sectors.
         </p>
       </div>
     );
--- pages/components/projects/UtilisationCSR.js
+++ pages/components/projects/UtilisationCSR.js
@@ -1,9 +1,9 @@
 const UtilisationCSR = () => {
   return (
     <div>
-      <h3 className="text-2xl font-semibold text-[#316b9e] mb-4">Better Utilisation of CSR Grants</h3>
+      <h3 className="text-2xl font-semibold text-[#316b9e] mb-4">Sub - Theme - 5</h3>
       <p className="text-lg text-gray-700">
-        Better utilisation of CSR grants for setting up homes and hostels for women and orphans; setting up old age homes, day care centres, and other facilities for senior citizens.
+      Better utilization of CSR grants for setting up homes and hostels for women and orphans; setting up old age homes, day care centers and such other facilities for senior citizens.
       </p>
     </div>
   );
