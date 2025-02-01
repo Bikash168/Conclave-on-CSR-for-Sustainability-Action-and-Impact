99425c58673bfa421ffbfd6130867fb586be52cf
Bikash Jethy <58800928+Bikash168@users.noreply.github.com>
2025-02-01 12:07:46 +0530

    new

--- pages/components/Contact.js
+++ pages/components/Contact.js
@@ -25,14 +25,7 @@ const Contact = () => {
           animate={{ scale: 1, opacity: 1 }}
           transition={{ duration: 0.6, ease: "easeOut" }}
         />
-        <motion.h2
-          className="text-4xl md:text-5xl font-bold mb-4 text-[#316b9e] hover:text-blue-700 transition-all duration-300"
-          initial={{ y: -50, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 0.6, delay: 0.2, ease: "easeOut" }}
-        >
-          Contact Us
-        </motion.h2>
+       
         <motion.h3
           className="text-3xl md:text-4xl font-semibold text-gray-700 mb-8"
           initial={{ y: -50, opacity: 0 }}
@@ -41,6 +34,15 @@ const Contact = () => {
         >
           Trident Academy of Technology
         </motion.h3>
+
+        <motion.h2
+          className="text-4xl md:text-5xl font-bold mb-4 text-[#316b9e] hover:text-blue-700 transition-all duration-300"
+          initial={{ y: -50, opacity: 0 }}
+          animate={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, delay: 0.2, ease: "easeOut" }}
+        >
+          Contact Us
+        </motion.h2>
 
         {/* Google Map */}
         <motion.div
