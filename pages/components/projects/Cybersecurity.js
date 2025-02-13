3fb4f3fb6bb55fd94f6ff2e35e24c78e30953ad5
Bikash Jethy <58800928+Bikash168@users.noreply.github.com>
2025-02-13 13:08:23 +0530

    new

--- pages/components/projects/Cybersecurity.js
+++ pages/components/projects/Cybersecurity.js
@@ -3,7 +3,7 @@ const Cybersecurity = () => {
     <div>
       <h3 className="text-2xl font-semibold text-[#316b9e] font-serif mb-4">Sub - Theme - 4</h3>
       <p className="text-lg text-gray-700 font-serif">
-      Emerging opportunities in Cyber Security, Privacy and Defense technologies. (OR) Future Frontiers: Emerging Opportunities in Cybersecurity, Privacy & Infosec Compliance
+      Cybersecurity Track: Emerging opportunities in Cyber Security, Privacy and Defense technologies. (OR) Future Frontiers: Emerging Opportunities in Cybersecurity, Privacy & Infosec Compliance (In partnership with M/s Threatsys)
       </p>
     </div>
   );
