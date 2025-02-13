704a7f75ddafd04c695eb7de3ac7740873b09f20
Bikash Jethy <58800928+Bikash168@users.noreply.github.com>
2025-02-13 13:05:32 +0530

    new

--- pages/components/projects/AI.js
+++ pages/components/projects/AI.js
@@ -3,7 +3,7 @@ const AI = () => {
     <div>
       <h3 className="text-2xl font-semibold text-[#316b9e] font-serif mb-4">Sub - Theme - 3</h3>
       <p className="text-lg text-gray-700 font-serif">
-      AI Hypergence: Opportunities & Challenges
+      Al Track: Al Hyper emergence: Opportunities & Challenges (In partnership with SPARC (P) Ltd.)
       </p>
     </div>
   );
--- pages/components/projects/Energy.js
+++ pages/components/projects/Energy.js
@@ -4,7 +4,6 @@ const Energy = () => {
         <h3 className="text-2xl font-semibold text-[#316b9e] font-serif mb-4">Sub - Theme - 2</h3>
         <p className="text-lg text-gray-700 font-serif">
         Energy Track: Innovative RE Integration: The Key to Resilient and Energy-Efficient Grids. (In partnership with SACRED Foundation)
-
       </p>
       </div>
     );
