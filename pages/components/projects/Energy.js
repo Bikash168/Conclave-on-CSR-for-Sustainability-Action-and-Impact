0f7987d144de04811bb56c117f6adcf4123c29ac
Bikash Jethy <58800928+Bikash168@users.noreply.github.com>
2025-02-13 13:04:38 +0530

    new

--- pages/components/projects/Energy.js
+++ pages/components/projects/Energy.js
@@ -3,7 +3,8 @@ const Energy = () => {
       <div>
         <h3 className="text-2xl font-semibold text-[#316b9e] font-serif mb-4">Sub - Theme - 2</h3>
         <p className="text-lg text-gray-700 font-serif">
-        Innovative RE Integration: The Key to Resilient and Energy-Efficient Grids
+        Energy Track: Innovative RE Integration: The Key to Resilient and Energy-Efficient Grids. (In partnership with SACRED Foundation)
+
       </p>
       </div>
     );
--- pages/components/projects/Semicon.js
+++ pages/components/projects/Semicon.js
@@ -3,7 +3,7 @@ const Semicon = () => {
     <div>
       <h3 className="text-2xl font-semibold text-[#316b9e] font-serif mb-4">Sub - Theme - 1</h3>
       <p className="text-lg text-gray-700 font-serif">
-      Exploring Innovations and Sustainability in Semiconductor Technologies leading to future opportunities (In Partnership with ARF Design Pvt Ltd. and VLSI Society of India)
+      Semicon Track: Exploring Innovations and Sustainability in Semiconductor Technologies leading to future opportunities (In Partnership with ARF Design Pvt Ltd. and VLSI Society of India)
       </p>
     </div>
   );
